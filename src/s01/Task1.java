2a2a3697f71173c6e6db7b7fb11837ca33610eb3
Anastasiia Iakimova <anastasiiaiakimova@MacBook-Pro.local>
2022-08-29 12:00:38 +0300

    Task1

---
+++ src/s01/Task1.java
@@ -0,0 +1,20 @@
+package s01;
+
+//возведение числа N в степень M;
+public class Task1 {
+    public static void main(String[] args) {
+
+        int N = 2;
+        int M = 10;
+
+        System.out.println(multiRecursive(N, M));
+
+    }
+
+    static public int multiRecursive(int N, int M) {
+        if (M == 0) {
+            return 1;
+        }
+        return N * multiRecursive(N, M - 1);
+    }
+}
