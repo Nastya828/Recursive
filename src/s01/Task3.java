c84e7171b6225e0904dc0bb5caac6f17ec67dce4
Anastasiia Iakimova <anastasiiaiakimova@MacBook-Pro.local>
2022-08-29 13:09:08 +0300

    Task3

---
+++ src/s01/Task3.java
@@ -0,0 +1,23 @@
+package s01;
+
+import java.util.LinkedList;
+
+
+//расчёт длины списка, для которого разрешена только одна операция удаления первого элемента pop(0);
+public class Task3 {
+    public static void main(String[] args) {
+        LinkedList<Integer> numbers = new LinkedList<>();
+        for (int i = 0; i < 11; i++) {
+            numbers.add(i);
+        }
+        System.out.println(sieListRec(numbers));
+    }
+
+    static public int sieListRec(LinkedList<Integer> numbers) {
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+        numbers.pop();
+        return 1 + sieListRec(numbers);
+    }
+}
