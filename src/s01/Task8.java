7e8ed0e8bfd6e31398e80cea5919e953b25a9b0c
Anastasiia Iakimova <anastasiiaiakimova@MacBook-Pro.local>
2022-08-30 13:33:54 +0300

    Task8

--- src/s01/Task8.java
+++ src/s01/Task8.java
@@ -1,8 +1,26 @@
 package s01;
-
 //8. поиск всех файлов в заданном каталоге, включая файлы, расположенные в подкаталогах произвольной вложенности.
-public class Task8 {
-    public static void main(String[] args) {
 
+import java.io.File;
+
+public class Task8 {
+
+    public static void main(String[] args) {
+        File file = new File(".");
+        searchFiles(file);
+    }
+
+    static void searchFiles(File file) {
+        if (file.isFile()) {
+            return;
+        }
+        File[] files = file.listFiles();
+        for (File f : files) {
+            if (f.isDirectory()) {
+                searchFiles(f);
+            } else {
+                System.out.println(f.getName());
+            }
+        }
     }
 }
