ab912bafa4bd15dce3f3868821f3a90931fd2e4e
Anastasiia Iakimova <anastasiiaiakimova@MacBook-Pro.local>
2022-08-31 13:37:35 +0300

    Tasks 7 2.1

--- src/s01/Task7.java
+++ src/s01/Task7.java
@@ -10,32 +10,30 @@ public class Task7 {
 
     public static void main(String[] args) {
         List<Integer> numbersList = new ArrayList<>();
-        numbersList.add(25);
+        numbersList.add(5);
         for (int i = 0; i < 18; i++) {
-            numbersList.add(i);
+            numbersList.add(7);
         }
 
-
-
         System.out.println(numbersList);
-        System.out.println("result =  " + searchSecondMax(numbersList, 0, 0, 0));
+
+        int max = Math.max(numbersList.get(0), numbersList.get(1));
+
+        System.out.println("result =  " + searchSecondMax(numbersList, 0, max, max));
     }
+
 
     static Integer searchSecondMax(List<Integer> inputList, int index, int max, int maxSecond) {
 
         if (index >= inputList.size()) {
             return maxSecond;
         }
-        if (index == 0) {
-            max = inputList.get(index);
-            maxSecond = inputList.get(index);
 
-        }
         if (inputList.get(index) > max) {
             maxSecond = max;
             max = inputList.get(index);
-
         }
+
         if ((inputList.get(index) < max) && (inputList.get(index) > maxSecond || maxSecond == max)) {
             maxSecond = inputList.get(index);
 
